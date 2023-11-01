e6c76d15981d0afa512912623882c4eff07e13b4
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2023-11-01 20:00:37 -0300

    Changes on get() from FileRepository. Avoid file download and we use StreamingBody to read the file content.

--- src/cloud/s3/FileRepository.py
+++ src/cloud/s3/FileRepository.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 from datetime import datetime
+from typing import List, Dict
+import json
 import boto3
 import os
 
@@ -13,25 +15,20 @@ class FileRepository:
             aws_secret_access_key=os.getenv('AWS_ACCESS_SECRET_KEY'), region_name='sa-east-1')
         self.__bucket = 'spotify-activity-data'
         self.__cloud_root_folder = 'raw/'
-        self.__local_root_folder = f"data/{self.__cloud_root_folder}"
     
     def __build_path_from_now(self) -> str:
         # raw/2023/10/31/
         return self.__cloud_root_folder + datetime.now().strftime('%Y-%m-%d').replace('-', '/')
 
-    def __build_path_from_key(self, key: str) -> str:
-        # raw/2023/10/31/00:00.json -> data/raw/2023_10_31_00-00.json
-        return self.__local_root_folder + \
-            key.split(self.__cloud_root_folder)[-1].replace('/', '_').replace(':', '-')
-
-    def get(self):
+    def get(self) -> List[Dict[str, List[Dict[str, str]]]]:
         # raw/2023/10/31/01:00.json, 02:00.json, ...
+        activity = []
         prefix = self.__build_path_from_now()
         day_content = self.__s3.list_objects(Bucket=self.__bucket, Prefix=prefix)
         for file in day_content['Contents']:
-            new_path = self.__build_path_from_key(file['Key'])
-            self.__s3.download_file(
-                Bucket=self.__bucket, Key=file['Key'], Filename=new_path)
+            activity.append(json.loads(self.__s3.get_object(
+                Bucket=self.__bucket, Key=file['Key'])['Body'].read().decode('utf-8')))
+        return activity
 
     # TODO: initially this FileRepository (maybe do one for raw, other for clean)
     # shouldn't have a post, since the data is gathered from the step-function, there should
