80f75ccf318ec4262ca16675bf54ce986ff41deb
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2023-11-02 21:12:57 -0300

    Change extract() a bit. We don't really care about dates on this point, and we just 'extract' the data from the crawler and 'extract' again from the Spotify API to gather the data.

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,41 +1,49 @@
 from src.cloud.s3.FileRepository import FileRepository
+from src.utils.SummarizedTrack import SummarizedTrack
 
 def extract():
     file_repository = FileRepository()
     raw_data = file_repository.get()
     
-    data_by_date = {} # <Date, list of songs played that day> (has repeated songs)
+    # Cleanup reports data, remove repeated songs from overlapped reports (e.g., 01:00, 02:00)
+    songs_data = {}
     for report in raw_data:
-        for date, date_songs in report.items():
-            if not data_by_date.get(date):
-                data_by_date[date] = []
-            data_by_date[date].extend(date_songs)
-    
-    # There's a lot of repeated stuff in data_by_date.
-    # Since in 1 hour it's very hard to listen to more than 50 songs.
-    # e.g: 01:00 and 02:00 report will probably have the same data, or a lot of repeated songs.
-    data = {} # <Date, List of songs played that day> (non-repeated)
-    for date, total_date_songs in data_by_date.items():
-        non_repeated_data = {}
-        for song in total_date_songs:
-            song_key = (song['track_id'], song['played_at'])
-            non_repeated_data[song_key] = song
-        data[date] = list(non_repeated_data.values())
-    
-    for date, songs in data.items():
-        print(f"date: {date}, songs played: {len(songs)}, songs listing")
-        for song in songs:
-            print(f"******* {song}")
+        for _, songs in report.items(): # <Date, List of songs>
+            for song in songs:
+                song_key = (song['track_id'], song['played_at'])
+                songs_data[song_key] = song
+    songs_data = list(songs_data.values())
+
+    summarized_tracks = []
+    for i, song in enumerate(songs_data):
+        #### testing 
+        summarized_track = SummarizedTrack(song)
+        summarized_tracks.append(summarized_track)
+        
+        x = summarized_track.get_sections_data()
+        print(summarized_track)
+        print(f"track sections: {len(x)}")
+        for section in x:
+            print(section)
+        print("**************************")
+        if i == 10: exit()
+        #### testing
     
     """
-        next steps ->
-        * what to do with files and dates*
-            - we've the songs played by date in case of overlaps, since 00:00, 01:00, 02:00 
-            reports will probably overlap with date N and N-1
-            - so our final dict, data, will have at most 2 different dates
-            - lookout what to do with the files, in the transform step, do we give a dataframe for the
-              date? or a batch of songs. -> TODO: look into this, we only transform stuff once.
-        * scan songs data with our data dict we just made *
-        * see what we do with segments/sections data, since those won't be the same size
-            for each song -> this will be normalized, but in the transform step, not here.*
+        We'll work with batches of data.
+
+        TODO: the way things are right now, we might process at most 50 songs x2 times.
+            ^ date n, report 00:00.
+              date n-1, report 23:00.
+            these two reports will probably share a lot of songs (at most 50), 
+            and will be processed in two different batches, meaning we might do 50 songs x2.
+            
+            ^ on load() we'll drop these repeated songs, but at that point the work is already done,
+            so might as well check for already processed songs in extract? but extract is supposed just
+            to extract and gather data.
+         
+        extract -> gather all non-repeated songs, run summarized information on them -> post them.
+        transform -> work with all the gathered data, work with sections data.
+        load -> post all the cleaned data into s3 again, check for dates and repeated data
+            from date n, n-1.
     """
