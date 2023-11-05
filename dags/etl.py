bc82d2ff60e91b3c2e18e4a0d71fa161cffb2f6c
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2023-11-05 20:40:35 -0300

    cleanup requirements, fix DAG code a bit? Will see how to deploy this into aws.

--- dags/etl.py
+++ dags/etl.py
@@ -1,17 +1,14 @@
+from src.etl.extract.extract import extract
+from src.etl.transform.transform import transform
+from src.etl.load.load import load
 from airflow.decorators import dag, task
-from src.extract.extract import extract
-from src.transform.transform import transform
-from src.load.load import load
 import pendulum
 
-# airflow is a pain to run on windows :(
 @dag(
     schedule="0 0 * * *", # probably will be every 1 hour
     start_date=pendulum.datetime(2023, 10, 24, tz="UTC-3"),
 )
 def run_etl():
-    """
-    """
 
     @task()
     def run_extract():
