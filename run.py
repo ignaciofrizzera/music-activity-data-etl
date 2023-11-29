aae46003c38a0a394c89a9b417a51908cc927ec3
IgnacioFrizzera <ignaciofrizzera@gmail.com>
2023-11-29 19:42:33 -0300

    update run.py

--- run.py
+++ run.py
@@ -1,12 +1,3 @@
-from src.extract.extract import extract
-from src.transform.transform import transform
-from src.load.load import load
-
+"""Use this file for debugging, running local code, testing, etc."""
 if __name__ == '__main__':
-    """ For the time being, testing like this. Once the ETL functions are done, will decide to make the 
-    airflow DAG or look into AWS resources to do everything there.
-    """
-    
-    extract()
-    # transform() -> TODO
-    # load() -> TODO
+    pass
