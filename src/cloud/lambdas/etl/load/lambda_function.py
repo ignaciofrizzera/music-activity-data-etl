730944224417893fe7c68a2e5012b10b1d239c2d
Ignacio Frizzera <ignaciofrizzera@gmail.com>
2024-01-07 19:33:34 -0300

    improvements on the Load function. We only post data when we actually have new data, and not everytime we run our ETL.

--- src/cloud/lambdas/etl/load/lambda_function.py
+++ src/cloud/lambdas/etl/load/lambda_function.py
@@ -13,21 +13,31 @@ def load():
         # "played_at": "2023-11-02 23:16"
         return played_at.split(' ')[0]
     
+    def __is_subset(small_df: pd.DataFrame, big_df: pd.DataFrame, ) -> bool:
+        # checks if the small_df is a subset of the big_df
+        return len(small_df.merge(big_df, how='inner')) == len(small_df)
+    
+    def __drop_and_post(data: pd.DataFrame, date_key) -> None:
+        data.drop_duplicates(['track_id', 'played_at'], inplace=True)
+        clean_file_repository.post(date_key, data.to_json(orient='records'))
+
     # Split songs by dates.
     songs_by_date = {}
     for song in data:
         song_date = __get_date_from_played_at(song['played_at'])
-        if not songs_by_date.get(song_date):
-            songs_by_date[song_date] = []
-        songs_by_date[song_date].append(song)
+        songs_by_date.setdefault(song_date, []).append(song)
     
     for date_key, songs_from_date in songs_by_date.items():
-        existing_df = clean_file_repository.get(date_key)
+        existing_data = clean_file_repository.get(date_key)
         new_data = pd.DataFrame(songs_from_date)
-        if not existing_df.empty:
-            new_data = pd.concat([existing_df, new_data])
-        new_data.drop_duplicates(['track_id', 'played_at'], inplace=True)
-        clean_file_repository.post(date_key, new_data.to_json(orient='records'))
+        
+        if existing_data.empty:
+            # we still haven't loaded any data for this date, so we upload new data
+            __drop_and_post(data=new_data, date_key=date_key)
+        elif not __is_subset(small_df=new_data, big_df=existing_data):
+            # the new data isn't a subset of the existing data, so we upload new data
+            new_data = pd.concat([existing_data, new_data])
+            __drop_and_post(data=new_data, date_key=date_key)
     
     # Once we generate our daily cleaned data, we proceed to delete all the intermediate files:
     # hourly reports, unstructured_report, structured_report.
