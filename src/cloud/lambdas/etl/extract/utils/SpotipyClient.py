df8543f8411421a41a227932f245ac13f2f6d7b8
ignaciofrizzera <ignaciofrizzera@gmail.com>
2026-01-17 16:13:56 -0300

    Cleanup SpotipyClient

--- src/cloud/lambdas/etl/extract/utils/SpotipyClient.py
+++ src/cloud/lambdas/etl/extract/utils/SpotipyClient.py
@@ -1,11 +1,19 @@
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-import spotipy
 import os
 
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import spotipy
+
+
 class SpotipyClient:
-    
+
     @staticmethod
     def general_data_client() -> spotipy.Spotify:
+        """
+        Initialize a general purpose client of Spotipy.
+
+        Returns:
+            spotipy.Spotify: Spotipy client instance.
+        """
         return spotipy.Spotify(
             client_credentials_manager=SpotifyClientCredentials(
                 client_id=os.environ.get('CLIENT_ID'),
